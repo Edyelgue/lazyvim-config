198f5767f8ed902dc83e98f5a3556a0c5d041896
Edyelgue Carneiro <edyelgue12@gmai.com>
2025-11-03 20:40:56 -0300

    adicionado formatters php

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -6,8 +6,7 @@ return {
     formatters_by_ft = {
       -- ... outras filetypes
       lua = { "stylua" },
-      php = { "blade-formatter", "phpcsfixer" }, -- Mantenha se quiser para PHP puro
-      -- ESTA LINHA É A CHAVE:
+      php = { "pint", "phpcsfixer" },
       blade = { "blade-formatter" },
     },
   },
